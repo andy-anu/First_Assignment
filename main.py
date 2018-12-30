c11adfed3b64733c2cf7c75aecc41f256c58806d
Anurag <8765anurag@gmail.com>
2018-12-31 00:23:23 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+Python Homework #1
+
+"""
+#Artist is a string variable
+artist = "Eminem"
+
+#year release is a integer variable
+yearReleased = 2010
+
+#genre is a string variable
+genre = "Hip Hop"
+
+#duration is a integer variable
+duration = 390
+
+#printing all variables
+print(artist)
+print(yearReleased)
+print(genre)
+print(duration)
